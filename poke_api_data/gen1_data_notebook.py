73c62e6ef5ea445626df47681ef6e100b58dd3db
fitzy1321 <32967490+fitzy1321@users.noreply.github.com>
2026-05-18 13:58:15 -0600

    finishing my json data splunking. Need to finish the script to take my compiled json and shove it into sqlite, then figure out my sqlite c code after that

--- poke_api_data/gen1_data_notebook.py
+++ poke_api_data/gen1_data_notebook.py
@@ -14,16 +14,20 @@
 
 import marimo
 
-__generated_with = "0.23.5"
+__generated_with = "0.23.6"
 app = marimo.App(width="columns")
 
 
 @app.cell(column=0)
-def utils():
+def _():
     import marimo as mo
     import requests
+    import json
+    import os
+    from pathlib import Path
+    from typing import Any
 
-    # import os
+
     @mo.persistent_cache
     def request_pokeapi(url: str):
         print("fetching data form api")
@@ -34,35 +38,29 @@ def utils():
             )
         return result.json()
 
+
+    def open_json_file(filename: str | Path) -> Any:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    def get_compiled_json(file_path: Path|str|None = None):
+        if not file_path:
+            file_path = Path(os.getcwd())
+        if not isinstance(file_path, Path):
+            file_path = Path(file_path)
+
+        if "poke_api_data" not in file_path.parts:
+            file_path  = file_path / "poke_api_data"
+
+        if "compiled_pokemon_data.json" not in file_path.parts:
+            file_path = file_path / "compiled_pokemon_data.json"
+
+        return open_json_file(file_path)
+
     return mo, request_pokeapi
 
 
-@app.cell(column=1)
-def _(request_pokeapi):
-    # mo.stop(
-    #     predicate=not gen_1_rbtn.value, output=mo.md("Click 👆 to run this cell")
-    # )
-
-    gen_1_data = request_pokeapi("https://pokeapi.co/api/v2/generation/1")
-    gen_1_data
-    return (gen_1_data,)
-
-
-@app.cell
-def _(gen_1_data):
-    move_urls = [m["url"] for m in sorted(gen_1_data["moves"], key=lambda x: x["name"])]
-    move_urls
-    return
-
-
-@app.cell
-def _(gen_1_data):
-    pokemon_urls = [p["url"] for p in gen_1_data["pokemon_species"]]
-    pokemon_urls
-    return
-
-
-@app.cell(hide_code=True)
+@app.cell(column=1, hide_code=True)
 def _(mo):
     mo.md(r"""
     The next 2 cells are to hit the individual pokemon endpoint.
@@ -82,8 +80,21 @@ def _(mo, poke_run_btn, request_pokeapi):
     # Stop execution if the button hasn't been clicked
     mo.stop(
         not poke_run_btn.value,
-        mo.md("Expensive network and parsing operation. Click 👆 to run this cell"),
+        mo.md(
+            "Expensive network and parsing operation. Click 👆 to run this cell"
+        ),
     )
+
+    # gen_1_data = request_pokeapi("https://pokeapi.co/api/v2/generation/1")
+    # gen_1_data
+
+    # move_urls = [
+    #     m["url"] for m in sorted(gen_1_data["moves"], key=lambda x: x["name"])
+    # ]
+    # move_urls
+
+    # pokemon_urls = [p["url"] for p in gen_1_data["pokemon_species"]]
+    # pokemon_urls
 
     poke_data = request_pokeapi("https://pokeapi.co/api/v2/pokemon/1")
     poke_data
@@ -102,7 +113,9 @@ def _(mo, move_run_btn, request_pokeapi):
     # Stop execution if the button hasn't been clicked
     mo.stop(
         not move_run_btn.value,
-        mo.md("Expensive network and parsing operation. Click 👆 to run this cell"),
+        mo.md(
+            "Expensive network and parsing operation. Click 👆 to run this cell"
+        ),
     )
 
     move_data = request_pokeapi("https://pokeapi.co/api/v2/move/1")
@@ -112,36 +125,10 @@ def _(mo, move_run_btn, request_pokeapi):
 
 @app.cell
 def _():
-    import sqlalchemy
+    # import sqlalchemy
 
-    DATABASE_URL = "sqlite:///pokedata.db"
-    engine = sqlalchemy.create_engine(DATABASE_URL)
-    return
-
-
-@app.cell
-def _():
-    return
-
-
-@app.cell(column=2)
-def _():
-    import marimo as mo
-
-    run_full_fetch_btn = mo.ui.run_button()
-    run_full_fetch_btn
-    return mo, run_full_fetch_btn
-
-
-@app.cell
-def _(mo, run_full_fetch_btn):
-    mo.stop(
-        not run_full_fetch_btn.value,
-        mo.md(
-            "This will run multiple get requests against the pokeapi. Do not run this often"
-        ),
-    )
-
+    # DATABASE_URL = "sqlite:///pokedata.db"
+    # engine = sqlalchemy.create_engine(DATABASE_URL)
     return
 
 
